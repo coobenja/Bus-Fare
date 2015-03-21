1f3a6e9d3603fb343927b17f5b0b18817cceb2e3
coobenja <coobenja@umich.edu>
2015-03-21 03:43:16 -0400

    SCREEN SHAKE FOR REAL FRIENDS

---
+++ Bus Fare, Nicklemancers/Assets/Scripts/CamShakeSimple.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamShakeSimple : MonoBehaviour 
+{
+	
+	Vector3 originalCameraPosition;
+	
+	float shakeAmt = 0;
+	public bool isShaking;
+	
+	public Camera mainCamera;
+	
+	public PlayerController playerController;
+	
+	void Start(){
+		originalCameraPosition = mainCamera.transform.position;
+		playerController = GetComponent<PlayerController> ();
+		isShaking = false;
+	}
+	
+	void Update(){
+		
+		if (playerController.kickColl.enabled == true && !isShaking) {
+			shakeAmt = .1f;
+			InvokeRepeating ("CameraShake", 0, .01f);
+			Invoke ("StopShaking", 0.3f);
+			isShaking = true;
+		} 
+	}
+	
+	/*void OnCollisionEnter2D(Collision2D coll) 
+	{
+		
+		shakeAmt = coll.relativeVelocity.magnitude * .0025f;
+		InvokeRepeating("CameraShake", 0, .01f);
+		Invoke("StopShaking", 0.3f);
+		
+	}*/
+	
+	void CameraShake()
+	{
+		if(shakeAmt>0) 
+		{
+			float quakeAmt = Random.value*shakeAmt*2 - shakeAmt;
+			Vector3 pp = mainCamera.transform.position;
+			pp.y+= quakeAmt; // can also add to x and/or z
+			mainCamera.transform.position = pp;
+		}
+	}
+	
+	void StopShaking()
+	{
+		CancelInvoke("CameraShake");
+		mainCamera.transform.position = originalCameraPosition;
+		isShaking = false;
+	}
+	
+}
