ed2f827c84a2ad77c01436c5330236bee3775a17
Becarls <becarls@umich.edu>
2015-03-21 13:17:07 -0400

    coins spout out now

--- Bus Fare, Nicklemancers/Assets/Scripts/CoinVelocity.cs
+++ Bus Fare, Nicklemancers/Assets/Scripts/CoinVelocity.cs
@@ -4,15 +4,21 @@ using System.Collections;
 public class CoinVelocity : MonoBehaviour {
 
 	public float speed;
-	private Vector2 coinspout = new Vector2 (0, 1.0f);
+	private Vector2 coinspout = new Vector2 (.01f * Random.Range (-500, 500), 5.0f);
+
 	//private float startHeight;
 
 	void Start () {
 		//startHeight = transform.position.y;
-
+		float startHeight = transform.position.y;
 		Rigidbody2D rigid;
 		rigid = GetComponent<Rigidbody2D>();
-		rigid.AddForce(coinspout, ForceMode2D.Force);
+		rigid.AddForce(coinspout * speed, ForceMode2D.Force);
+		if(transform.position.y <= (startHeight - 2f)) {
+			
+			GetComponent<Rigidbody2D>().gravityScale = 0f;
+
+		}
 
 	}
 	
