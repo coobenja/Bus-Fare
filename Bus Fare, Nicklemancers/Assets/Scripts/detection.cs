d99edd1161527acd410d3f5f0bbc0fc334200f13
Becarls <becarls@umich.edu>
2015-03-20 22:36:03 -0400

    beep boop

---
+++ Bus Fare, Nicklemancers/Assets/Scripts/detection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class detection : MonoBehaviour {
+	public Transform player;
+	public float detect;
+	public float escape;
+	private float speed = .1f;
+
+	public bool chasing;
+	// Update is called once per frame
+	void Update () {
+
+
+		float dist = Vector2.Distance(player.position, transform.position);
+		Debug.Log (dist);
+		 if (dist <= detect) {
+			chasing = true;
+		}
+
+		if (dist >= escape) {
+			chasing = false;
+		}
+
+		if (chasing) {
+			transform.position = Vector2.MoveTowards(transform.position, player.position, speed);
+		}
+
+
+	
+	}
+}
