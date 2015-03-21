cc97e7e8bdd41d56bdf822f831b3f9a642a5ef14
Becarls <becarls@umich.edu>
2015-03-21 03:13:36 -0400

    Coins exist now, so thats cool

--- Bus Fare, Nicklemancers/Assets/Scripts/NPCCoins.cs
+++ Bus Fare, Nicklemancers/Assets/Scripts/NPCCoins.cs
@@ -8,7 +8,7 @@ public class NPCCoins : MonoBehaviour {
 	public GameObject quarter;
 
 	private float coins;
-	private bool chance = (Random.value < .5);
+	private bool chance;
 	// Use this for initialization
 	void Start () {
 		coins = Random.Range (2, 30);
@@ -17,28 +17,28 @@ public class NPCCoins : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
+		chance = Random.value > .50;
+		if (other.gameObject.tag == "Foot") {
+			coins = coins - 25;
+			Rigidbody2D deathCash;
+			deathCash = Instantiate (quarter, transform.position, transform.rotation) as Rigidbody2D;
+			deathCash.velocity = transform.TransformDirection (Vector2.up * 10);
+		}
 		if (other.gameObject.tag == "Fist") {
 			if (chance) {
 				coins = coins - 5;
 				Rigidbody2D deathCash;
-				deathCash = Instantiate(nickel, transform.position, transform.rotation) as Rigidbody2D;
-				deathCash.velocity = transform.TransformDirection(Vector2.up * 10);
-			}
+				deathCash = Instantiate (nickel, transform.position, transform.rotation) as Rigidbody2D;
+				deathCash.velocity = transform.TransformDirection (Vector2.up * 10);
+			} 
 			else {
 				coins = coins - 10;
 				Rigidbody2D deathCash;
-				deathCash = Instantiate(dime, transform.position, transform.rotation) as Rigidbody2D;
-				deathCash.velocity = transform.TransformDirection(Vector2.up * 10);
+				deathCash = Instantiate (dime, transform.position, transform.rotation) as Rigidbody2D;
+				deathCash.velocity = transform.TransformDirection (Vector2.up * 10);
 			}
-		if (other.gameObject.tag == "Foot") {
-				coins = coins - 25;
-				Rigidbody2D deathCash;
-				deathCash = Instantiate(quarter, transform.position, transform.rotation) as Rigidbody2D;
-				deathCash.velocity = transform.TransformDirection(Vector2.up * 10);
-			}
-	}
-		
+		}
+		Debug.Log (other.gameObject.tag == "Foot");
 
-
-}
+	}			
 }
