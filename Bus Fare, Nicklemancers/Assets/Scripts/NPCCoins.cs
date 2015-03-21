853f53f15b31ed6a90ea3f1953c73b3caa7e78b6
Becarls <becarls@umich.edu>
2015-03-21 01:35:43 -0400

    I did some things with NPC's I think

---
+++ Bus Fare, Nicklemancers/Assets/Scripts/NPCCoins.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCCoins : MonoBehaviour {
+
+	private float coins;
+	// Use this for initialization
+	void Start () {
+		coins = Random.Range (2, 30);
+		coins = coins * 5;
+		Debug.Log (coins);
+	}
+
+
+}
