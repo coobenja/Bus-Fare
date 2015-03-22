f83ec644ccbf4d4a9ff9b5c7cb7429ae1e962036
Ann Ho <kjuri27@yahoo.com>
2015-03-22 11:50:33 -0400

    Title Complete (w/o sound)

---
+++ Bus Fare, Nicklemancers/Assets/Scripts/TitleScripts/LevelSwitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitch : MonoBehaviour {
+//	public bool restartTitleScreen = false;
+//	public float timeTilRestart;
+	public string levelToLoad;
+//	private float timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Debug.Log ("START");
+			Application.LoadLevel (levelToLoad);
+		}
+		//Debug.Log (Input.GetButtonDown (button));
+		// Change this to change to a different level when start is pressed
+	}
+}
---
+++ Bus Fare, Nicklemancers/Assets/Scripts/TitleScripts/TitleFadeIn.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleFadeIn : MonoBehaviour {
+	public float startDelay;
+	public float flashSpeed;
+	public bool repeat = false;
+
+	private bool fadeIn = true;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = new Color (1f, 1f, 1f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		flash ();
+	}
+
+	void flash()
+	{
+		Color on = new Color (1f, 1f, 1f, 0.95f);
+		Color off = new Color (1f, 1f, 1f, 0f);
+
+		if (flashSpeed == 0)
+			flashSpeed = 1;
+
+		if (startDelay <= 0) 
+		{
+			if (fadeIn)
+			{
+				GetComponent<Renderer>().material.color = Color.Lerp (GetComponent<Renderer>().material.color, on, Time.deltaTime * flashSpeed);
+				if (GetComponent<Renderer>().material.color == on)
+					fadeIn = false;
+			}
+//			else
+//			{
+//				GetComponent<Renderer>().material.color = Color.Lerp (GetComponent<Renderer>().material.color, off, Time.deltaTime * flashSpeed);
+//				if (GetComponent<Renderer>().material.color == off && repeat)
+//				    fadeIn = true;
+//			}
+		}
+		else
+		{
+			startDelay -= Time.deltaTime;
+		}
+	}
+}
