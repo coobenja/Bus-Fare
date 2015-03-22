7f002b10530cffc9c31770a52e3297defb040b44
Ann Ho <kjuri27@yahoo.com>
2015-03-22 01:58:00 -0400

    WIP Title Scene Added

---
+++ Bus Fare, Nicklemancers/Assets/Scripts/TitleScripts/TimedCamShake.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedCamShake : MonoBehaviour 
+{
+	
+	Vector3 originalCameraPosition;
+	private bool isShaking;
+
+	public float shakeSpeed;
+	public float shakeAmt = 0;
+	public Camera mainCamera;
+	public float startDelay;
+	public float shakeDuration;
+
+	
+	public PlayerController playerController;
+	
+	void Start(){
+		//originalCameraPosition = mainCamera.transform.position;
+		playerController = GetComponent<PlayerController> ();
+		isShaking = false;
+	}
+	
+	void Update(){
+		if (startDelay <= 0 && !isShaking) 
+		{
+			//shakeAmt = .05f;
+			InvokeRepeating ("CameraShake", 0, .01f);
+			Invoke ("StopShaking", shakeDuration);
+			isShaking = true;
+		}
+		else if (startDelay > 0)
+		{
+			startDelay -= Time.deltaTime;
+		}
+	}
+	
+	/*void OnCollisionEnter2D(Collision2D coll) 
+	{
+		
+		shakeAmt = coll.relativeVelocity.magnitude * .0025f;
+		InvokeRepeating("CameraShake", 0, .01f);
+		Invoke("StopShaking", 0.3f);
+		
+	}*/
+	
+	void CameraShake()
+	{
+		if(shakeAmt>0) 
+		{
+			float quakeAmt = Random.value*shakeAmt*2 - shakeAmt;
+			Vector3 pp = mainCamera.transform.position;
+			pp.y+= quakeAmt; // can also add to x and/or z
+			pp.z+= quakeAmt;
+			mainCamera.transform.position = pp;
+		}
+	}
+	
+	void StopShaking()
+	{
+		CancelInvoke("CameraShake");
+		//mainCamera.transform.position = originalCameraPosition;
+		isShaking = false;
+	}
+	
+}
---
+++ Bus Fare, Nicklemancers/Assets/Scripts/TitleScripts/TitleAnimation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleAnimation : MonoBehaviour {
+
+	public float newPositionX;
+	public float newPositionY;
+	public float newPositionZ;
+	public float moveSpeed;
+	public float startDelay;
+
+	// Use this for initialization
+	void Start () {
+		//transform.position = new Vector3(transform.position.x, -13, transform.position.z);
+	}
+	
+	void move()
+	{
+		Vector3 newPosition = transform.position;
+
+		if (moveSpeed == 0)
+			moveSpeed = 1;
+		if (newPositionX != 0)
+			newPosition = new Vector3 (newPositionX, newPosition.y, newPosition.z);
+		if (newPositionY != 0)
+			newPosition = new Vector3 (newPosition.x, newPositionY, newPosition.z);
+		if (newPositionZ != 0)
+			newPosition = new Vector3 (newPosition.x, newPosition.y, newPositionZ);
+		if (startDelay <= 0) 
+		{
+			if (transform.position != newPosition)
+			{
+				transform.position = Vector3.Lerp (transform.position, newPosition, Time.deltaTime * moveSpeed);
+			}
+		}
+		else
+		{
+			startDelay -= Time.deltaTime;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move ();
+	}
+}
