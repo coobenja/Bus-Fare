b12c26d74ba22fae6b2f5e71281f5eb1a53b056f
Becarls <becarls@umich.edu>
2015-03-22 10:45:38 -0400

    hope this works

--- Bus Fare, Nicklemancers/Assets/Scripts/HopeThisWorksGui.cs
+++ Bus Fare, Nicklemancers/Assets/Scripts/HopeThisWorksGui.cs
@@ -8,7 +8,7 @@ public class HopeThisWorksGui : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		playerController = GameObject.Find("Player").GetComponent<PlayerController> ();
-		//gui.text = "It's $5.00 to ride the bus, if you have the money press 'R'.";
+		//gui.text = "It's $5.00 to ride the bus, if you have the money press 'B', for Bus.";
 	}
 	
 	// Update is called once per frame
@@ -21,7 +21,7 @@ public class HopeThisWorksGui : MonoBehaviour {
 			
 			if (Input.GetKeyDown(KeyCode.R)){
 				//gui.text = "You Have Stolen:" + playerController.coins + "cents";
-				Application.LoadLevel("Game Over");
+				Application.LoadLevel("GameOver");
 			}
 }
 }
